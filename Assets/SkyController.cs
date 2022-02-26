cb6ffd1058de015b3003edfbfe2a0fda2521a20b
Maxim.Kostyukov <maxim.kostyukov@incari.com>
2022-02-26 18:10:29 +0100

    implemented colorpallete for sky

---
+++ Assets/SkyController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyController : MonoBehaviour
+{
+    [SerializeField]
+    private ColorPalette colorPalette;
+
+    private MeshRenderer mRenderer;
+    private MaterialPropertyBlock mBlock;
+
+    private void Awake()
+    {
+        mRenderer = GetComponent<MeshRenderer>();
+        mRenderer.GetPropertyBlock(mBlock);
+
+        mBlock.SetColor("_MainSkyColor", colorPalette.getMainSkyColor());
+        mBlock.SetColor("_FadeSkyColor", colorPalette.getFadeSkyColor());
+
+        mRenderer.SetPropertyBlock(mBlock);
+    }
+}
